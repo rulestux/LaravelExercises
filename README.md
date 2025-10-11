d339f5189b775b20e4a31d20896bb77f2cafeb4b
rulestux <rulestux@gmail.com>
2025-10-11 20:03:43 -0300

    Optional parameters and default values.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Exercises from the Laravel course by Jorge Sant'Ana on the Udemy platform.
 
-All exercises are completed using Vim or NeoVim, with plugins such as Conquer of Completion for PHP with 'CoC PHP Language Server'.
+All exercises are completed using Vim or NeoVim, with plugins such as Conquer of Completion for PHP with 'CoC-PHP Language Server'.
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -7,17 +7,26 @@ Route::get('/', function () {
     return view('welcome');
 });
  */
-
+// as rotas direcionam para os 'controllers' e os 'controllers' redirecionam
+// as requisições para a 'resources/views/site/':
 Route::get('/', [\App\Http\Controllers\MainController::class,'main']);
 
 Route::get('/about-us', [\App\Http\Controllers\AboutUsController::class, 'aboutUs']);
 
 Route::get('/contact', [\App\Http\Controllers\ContactController::class, 'contact']);
 
-
+// usa-se '?' para indicar ao Laravel que o parâmetro é opcional; atribuem-se
+// valores padrões na função de 'callback' para que não fiquem vazios, caso
+// não sejam preenchidos pelo usuário final:
 Route::get(
-    '/contact/{name}/{category}/{subject}/{message}',
-    function(string $name, string $category, string $subject, string $message) {
-        echo 'Estamos aqui: $name - $category - $subject - $message';
+    '/contact/{name?}/{category?}/{subject?}/{message?}',
+    // função de 'callback':
+    function(
+        string $name = 'Unknown',
+        string $category = 'Information',
+        string $subject = 'Contact',
+        string $message = 'Message not written'
+    ) {
+        echo "Estamos aqui: $name - $category - $subject - $message";
     }
 );
